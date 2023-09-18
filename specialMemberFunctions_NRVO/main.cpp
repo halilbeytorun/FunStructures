275e2da58733adb79786ef693b765f0e6612ff33
halilbeytorun <beytorun25@gmail.com>
2023-09-18 19:12:48 +0300

    NRVO fix

--- specialMemberFunctions_NRVO/main.cpp
+++ specialMemberFunctions_NRVO/main.cpp
@@ -1,9 +1,51 @@
-#include<iostream>
+#include <iostream>
+
+
+class Myclass
+{
+public:
+	Myclass()
+	{
+		std::cout << "default ctor\n";
+	}
+	Myclass(const Myclass&)
+	{
+		std::cout << "copy ctor\n";
+	}
+	Myclass(Myclass&&)
+	{
+		std::cout << "move ctor\n";
+	}
+
+	Myclass& operator=(const Myclass&)
+	{
+		std::cout << "copy assignment operator\n";
+		return *this;
+	}
+	Myclass& operator=(Myclass&&)
+	{
+		std::cout << "move assignment operator\n";
+		return *this;
+
+	}
+
+};
+
+Myclass foo()
+{
+	Myclass m;
+	return m;
+}
+
 
 
 int main()
 {
+	//Myclass a;
+	//a = foo();
+	//foo();
 
+	Myclass a = foo();
+	//foo();
 
-	return 0;
 }
