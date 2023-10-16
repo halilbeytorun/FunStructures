2d8f1d4e80c73c7846bc151d4a063cb87d75d51a
halilbeytorun <beytorun25@gmail.com>
2023-10-17 00:52:20 +0300

    Conversion Ctor example, first conversion ctor is called, then copy or move assignment operator will be called. No mandatory optimization here.

---
+++ conversionCtor/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+
+class A
+{
+public:
+	A()
+	{
+		std::cout << "Default ctor\n";
+	}
+	A(int)
+	{
+		std::cout << "conversion ctor\n";
+	}
+	A& operator=(const A& other)
+	{
+		std::cout << "copy assignment operator\n";
+		return *this;
+	}
+	// If this is commented, copy assignment operator would be called.
+	A& operator=(A&& other)
+	{
+		std::cout << "move assignment operator\n";
+		return *this;
+	}
+
+};
+
+int main()
+{
+	A a;
+	a = 1;
+}
