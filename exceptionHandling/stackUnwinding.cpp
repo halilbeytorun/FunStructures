429f20a7e1d90bdf92071482c467c51de5284d32
halilbeytorun <beytorun25@gmail.com>
2023-11-09 18:47:50 +0300

    stack unwinding example, RAII importance

---
+++ exceptionHandling/stackUnwinding.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <typeinfo>
+#include <cassert>
+#include <exception>
+
+
+class A
+{
+public:
+	A()
+	{
+		std::cout << this << "  A address resource is taken\n";
+	}
+	~A()
+	{
+		std::cout << this << "  A address resource is given\n";
+	}
+};
+
+class B
+{
+public:
+	B()
+	{
+		std::cout << this << "  B address resource is taken\n";
+	}
+	~B()
+	{
+		std::cout << this << "  B address resource is given\n";
+	}
+};
+
+
+void f4()
+{
+	A a1;
+	B b1;
+	throw std::exception();
+}
+void f3()
+{
+	B b1;
+	f4();
+}
+void f2()
+{
+	A a1;
+	f3();
+}
+void f1()
+{
+	f2();
+}
+
+
+int main()
+{
+	try
+	{
+		f1();
+	}
+	// destructors of the object created are called before going in here. If there would not be a cath function, 
+	//	std::terminate function would call abort function and destructors would not be called. Notice the importance of RAII
+	catch (const std::exception&)
+	{
+		(void)getchar();
+	}
+}
+
