271aa0a0940d5d6b4403d4a8d29e7f5f3a1eadf3
halilbeytorun <beytorun25@gmail.com>
2023-11-27 09:36:33 +0300

    added variable template example

---
+++ template/variableTemplate.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <type_traits>
+
+template<typename T>
+constexpr auto pi = (T)3.14159265359;
+
+template<typename T>
+struct isLValueRef : std::false_type
+{};
+template<typename T>
+struct isLValueRef<T&> : std::true_type
+{};
+template<typename T>
+constexpr bool isLValueRef_v = isLValueRef<T>::value;
+
+template<typename T>
+void foo(T x)
+{
+	static_assert(!std::is_pointer_v<T> || !std::is_reference_v<T>, "does not accept pointer or referance.");
+}
+
+
+int main()
+{
+	constexpr auto x = pi<double>;
+	int arr[pi<int>]{};
+	constexpr auto val = isLValueRef_v<bool>;
+	foo(arr);
+}
