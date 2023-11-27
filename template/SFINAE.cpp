0a47f8a84e22975f7d4610245b8947316ea9be4e
halilbeytorun <beytorun25@gmail.com>
2023-11-27 09:51:09 +0300

    enable_if examples, its 3 places to use in SFINAE

--- template/SFINAE.cpp
+++ template/SFINAE.cpp
@@ -31,6 +31,23 @@ template<typename T>
 void func(T** x) {}
 
 
+// with that writing, selection is done before specialization.. SFINAE is used!
+// If T is not pointer, enable_if's false specialization will be selected which does not have type which would caluse substitution failure...
+// Just change std::is_pointer_v according to your constraint to use SFINAE and enable_if. (like std::is_same, std::is_integral ..)
+template<typename T>
+std::enable_if_t<std::is_pointer_v<T>> enable_if_return_func(T) {}
+void enable_if_return_func(double) {}
+
+
+template<typename T, typename U = std::enable_if_t<std::is_pointer_v<T>, T>>
+void enable_if_template_param_func(T) {}
+void enable_if_template_param_func(double) {}
+
+template<typename T>
+void enable_if_function_param_func(T x, std::enable_if_t<std::is_pointer_v<T>>* = nullptr) {}	// second param void pointer
+void enable_if_function_param_func(double) {}
+
+
 int main()
 {
 	foo(2);	// observe which one is called.
@@ -44,5 +61,10 @@ int main()
 	func(&x);	// calls second function
 	int* ptr = &x;
 	func(&ptr);	// calls third function
+
+
+	enable_if_return_func(12);	// SFINAE d out
+	enable_if_template_param_func(12);// SFINAE d out
+	enable_if_function_param_func(12);// SFINAE d out
 
 }
