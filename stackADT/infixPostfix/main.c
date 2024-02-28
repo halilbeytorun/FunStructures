f05d4384394635c4b7d98fd7bee10e717e2a2f6f
halilbeytorun <beytorun25@gmail.com>
2024-02-28 20:29:04 +0300

    more compheransive infix to postfix conversion

--- stackADT/infixPostfix/main.c
+++ stackADT/infixPostfix/main.c
@@ -1,104 +1,105 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-struct Node
-{
-    char data;
-    struct Node *next;
-}*top=NULL;
-
-void push(char x)
-{
-    struct Node *t;
-    t = (struct Node*)malloc(sizeof(struct Node));
-    if(!t)
-        printf("stack is full\n");
-    else
-    {
-        t->data = x;
-        t->next = top;
-        top = t;
-    }
-}
-
-char pop()
-{
-    struct Node *t;
-    char x = -1;
-    if(top == NULL)
-        printf("Stack is Empty\n");
-    else
-    {
-        t = top;
-        top = top->next;
-        x = t->data;
-        free(t);
-    }
-    return x;
-}
-
-void Display()
-{
-    struct Node *p;
-    p = top;
-    while(p)
-    {
-        printf("%c ", p->data);
-        p=p->next;
-    }
-    printf("\n");
-}
-
-int presedence(char x)
-{
-    if(x == '+' || x == '-')
-        return 1;
-    else if(x== '*' || x == '/')
-        return 2;
-    return 0;
-}
-int isOperand(char x)
-{
-    if(x == '+' || x == '-' || x == '*' || x == '/')
-        return 0;
-    else
-        return 1;
-}
-
-char* InToPost(char *infix)
-{
-    int i=0, j=0;
-    char *postfix;
-    int len = strlen(infix);
-    postfix = (char *) malloc((len+2) * sizeof(char));
-
-    while(infix[i] != '\0')
-    {
-        if(isOperand(infix[i]))
-            postfix[j++] = infix[i++];
-        else
-        {
-            if(presedence(infix[i]) > presedence(top->data))
-                push(infix[i++]);
-            else
-            {
-                postfix[j++] = pop();
-            }
+    #include<stdio.h>
+    #include<stdlib.h>
+    #include<string.h>
+     
+    struct Stack{
+        int size;
+        int top;
+        char *S;
+    };
+     
+    void push(struct Stack *st, char x){
+        if(st->top == st->size - 1)
+            printf("Stack Overflow\n");
+        else{
+            st->top++;
+            st->S[st->top] = x;
         }
     }
-    while(top != NULL)
-    {
-        postfix[j++] = pop();
+     
+    char pop(struct Stack *st){
+        if(st->top == -1)
+            printf("Stack Underflow");
+        else{
+            return st->S[st->top--];
+        }
     }
-    postfix[j] = '\0';
-    return postfix;
-}
-
-int main()
-{
-    char*infix = "a+b*c-d/e";
-    push('#');  // needed to not get any error in InToPost function, nullptr exception..
-    char* postfix = InToPost(infix);
-    printf("%s\n", postfix);
-}
+     
+    int isOperand(char x){
+        if(x == '+' || x == '-' || x == '/' || x == '*' || x =='(' || x ==')' || x == '^' )
+            return 0;
+        else
+            return 1;
+    }
+     
+    int outPre(char x){
+        if(x == '+' || x == '-')
+            return 1;
+        else if( x == '/' || x == '*')
+            return 3;
+        else if(x == '^')
+            return 6;
+        else if(x == '(')
+            return 7;
+        else if(x == ')')
+            return 0;
+        else
+            return -1;
+    }
+     
+    int inPre(char x){
+        if(x == '+' || x == '-')
+            return 2;
+        else if( x == '/' || x == '*')
+            return 4;
+        else if(x == '^')
+            return 5;
+        else if(x == '(')
+            return 0;
+        else
+            return -1;
+    }
+     
+    char * InToPost(char *infix){
+        int len = strlen(infix);
+        int i = 0, j = 0;
+        struct Stack st;
+        char *postfix = (char *)malloc((len + 1)* sizeof(char));
+        st.size = len;
+        st.top = -1;
+        st.S = (char *)malloc(st.size * sizeof(char));
+        
+        while(infix[i] != '\0'){
+            if(isOperand(infix[i])){
+                postfix[j++] = infix[i++];
+            }
+            else{
+                if(st.top == -1 || outPre(infix[i]) > inPre(st.S[st.top]))
+                    push(&st, infix[i++]);
+                else{
+                    if(outPre(infix[i]) == inPre(st.S[st.top])){
+                        pop(&st);
+                        i++;
+                    }
+                    else
+                        postfix[j++] = pop(&st);
+                }
+            }
+        }
+        while(st.top != -1){
+            postfix[j++] = pop(&st);
+        }
+        
+        postfix[j] = '\0';
+        return postfix;
+        
+        
+    }
+     
+    void main(){
+        char *infix = "((a+b)*c)-d^e^f";
+        
+        char *postfix = InToPost(infix);
+        
+        printf("\n%s",postfix);
+    }
