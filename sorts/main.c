5536dcba01c7930281b50968c33f6b2912944a22
halilbeytorun <beytorun25@gmail.com>
2024-05-02 11:55:13 +0300

    implement selection sort

--- sorts/main.c
+++ sorts/main.c
@@ -49,10 +49,25 @@ void Insertion(int A[], int n)
     }
 }
 
+void Selection(int A[], int n)
+{
+    int i, j, k;
+    
+    for(i = 0; i < n-1; i++)
+    {
+        for(j=k=i; j < n; j++)
+        {
+            if(A[j] < A[k])
+                k = j;
+        }
+        swap(&A[i], &A[k]);
+    }
+}
+
 int main()
 {
     int A[] = {3, 7, 9, 10, 6, 5, 12, 4, 11, 2}, n=10;
-    Insertion(A, 10);
+    Selection(A, 10);
     for(int i = 0; i < 10; i++)
     {
         printf("%d ", A[i]);
