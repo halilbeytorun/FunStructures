8f52ae94eb231b073bf3f6f9f16fe582fec98daa
halilbeytorun <beytorun25@gmail.com>
2024-05-04 10:50:37 +0300

    implement quick sort

--- sorts/main.c
+++ sorts/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void swap(int * x, int *y)
+void swap(int *x, int *y)
 {
     int temp = *x;
     *x = *y;
@@ -64,11 +64,50 @@ void Selection(int A[], int n)
     }
 }
 
+int Partition(int A[], int l, int h)
+{
+    // pivot finding line, if middle pivot want to be used, than middle element can be swapped with the first element..
+    int pivot = A[l];
+    int i = l;
+    int j = h;
+    do
+    {
+        do {i++;} while (i < h && A[i] <= pivot);// TODO
+        do {j--;} while (A[j] > pivot); // > is used this side because we want for j to stop at pivot index in the worst case.
+        if(i < j)
+            swap(&A[i], &A[j]);
+        
+    } while (i < j);
+    swap(&A[j], &A[l]);
+    return j;
+}
+
+void QuickSort(int A[], int n)
+{
+    if(1 == n)
+        return;
+    int nextPart = 
+    Partition(A, 0, n);
+    if(0 == nextPart)
+        QuickSort(A+1, n-1);
+    else if(n-1 == nextPart)
+    {
+        QuickSort(A, n-1);
+    }
+    else
+    {
+        QuickSort(A, nextPart);
+        QuickSort(A + nextPart + 1, n - nextPart - 1);
+    }
+}
+
+
 int main()
 {
-    int A[] = {3, 7, 9, 10, 6, 5, 12, 4, 11, 2}, n=10;
-    Selection(A, 10);
-    for(int i = 0; i < 10; i++)
+    int A[] = {11, 13, 7, 12, 16, 9, 24, 5, 10, 3}, n=sizeof(A)/ sizeof(int);
+    //int A[] = {3, 1, 2, 4}, n=sizeof(A)/ sizeof(int);
+    QuickSort(A, n);
+    for(int i = 0; i < n; i++)
     {
         printf("%d ", A[i]);
     }
